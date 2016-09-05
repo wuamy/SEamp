2b982cf4829caf3341128a957547a850e3a454c8
amy <wuamyamy@gmail.com>
2016-09-05 17:56:12 -0400

    update the code from home

---
+++ src/main/java/appModules/SignIn_Action.java
@@ -0,0 +1,18 @@
+package appModules;
+
+import org.testng.Reporter;
+import pageObjects.Home_Page;
+import pageObjects.LogIn_Page;
+import utility.Constant;
+import utility.ExcelUtils;
+import utility.Log;
+import utility.Utils;
+
+/**
+ * Created by wu on 9/5/2016.
+ */
+// This is called Modularization, when we club series of actions in to one
+// Module. for Modular driven framework, please see
+// http://toolsqa.wpengine.com/selenium-webdriver/modular-driven/
+public class SignIn_Action {
+}
