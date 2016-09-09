ed8d5bc65f92a1b69f079e2666ed238f650277b8
wuamy <wuamyamy@gmail.com>
2016-09-09 08:07:00 -0400

    edit it from office

--- src/main/java/appModules/SignIn_Action.java
+++ src/main/java/appModules/SignIn_Action.java
@@ -15,4 +15,14 @@ import utility.Utils;
 // Module. for Modular driven framework, please see
 // http://toolsqa.wpengine.com/selenium-webdriver/modular-driven/
 public class SignIn_Action {
+    //iTestcaseRow is the row number of our Testcase name in the test data sheet
+    //iTestcaseRow is passed as an argument to this method, so that it can use inside this method
+    // for use of functions and parameters, pleas see http://toolsqa.wpengine.com/selenium-webdriver/function-parameters/
+    public static void Execute(int iTestCaseRow) throws Exception{
+        //Click on the My Account link on the Home Page
+        Home_Page.lnk_MyAccout().click;
+        Log.info("Click action is performed on My Account link");
+        //store the UserName in to a String variable and getting the UserName from Test Data excel sheet
+        //
+    }
 }
