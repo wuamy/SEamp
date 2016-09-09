bf4e7030e51b138da821dc42a6745d7fa8df9382
wuamy <wuamyamy@gmail.com>
2016-09-09 08:48:59 -0400

    edit it from office

---
+++ src/main/java/utility/Constant.java
@@ -0,0 +1,46 @@
+package utility;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+
+/**
+ * Created by amy on 9/9/2016.
+ */
+public class Constant {
+    public static final String URL = "http://store.demoqa.com/";
+    public static final String Username = "amy";
+    public static final String Password = "";
+    public static final String Path_TestData = "D://ToolsQA//OnlineStore//src//testData//";
+    public static final String File_TestData = "TestData.xlsx";
+
+    //test data sheet column
+    public static final int Col_TestCaseName = 0;
+
+    public static final int Col_UserName =1 ;
+
+    public static final int Col_Password = 2;
+
+    public static final int Col_Browser = 3;
+
+    public static final int Col_ProductType = 4;
+
+    public static final int Col_ProductNumber = 5;
+
+    public static final int Col_FirstName = 6;
+
+    public static final int Col_LastName = 7;
+
+    public static final int Col_Address = 8;
+
+    public static final int Col_City = 9;
+
+    public static final int Col_Country = 10;
+
+    public static final int Col_Phone = 11;
+
+    public static final int Col_Email = 12;
+
+    public static final int Col_Result = 13;
+
+    public static final String Path_ScreenShot = "D://ToolsQA//OnlineStore//src//Screenshots//";
+
+}
