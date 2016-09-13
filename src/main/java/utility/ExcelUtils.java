c0f4a5a9f027aae65c95cab912f074274a2558d0
wuamy <wuamyamy@gmail.com>
2016-09-13 11:54:56 -0400

    edit it from office

--- src/main/java/utility/ExcelUtils.java
+++ src/main/java/utility/ExcelUtils.java
@@ -1,6 +1,8 @@
 package utility;
 
 import java.io.FileInputStream;
+
+import org.apache.commons.logging.Log;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -16,5 +18,22 @@ public class ExcelUtils {
     private static XSSFRow Row;
 
     //This method is to set the file path and to open the excel file
+    //Pass Excel path and sheetname as arguments to this method
+    public static void setExcelFile(String Path, String SheetName) throws Exception{
+        try {
+            //open the excel file
+            FileInputStream ExcelFile = new FileInputStream(Path);
+            //Access the required test data sheet
+            EXcelWBook = new XSSFWorkbook(ExcelFile);
+            ExcelWSheet = EXcelWBook.getSheet(SheetName);
+            Log.info("Excel sheet opened!");
+        } catch (Exception e){
+            throw e;
+        }
+
+    }
+
+
+
 
 }
