530c494d243e203f044113dc646edbd9c0ce29d5
amy <wuamyamy@gmail.com>
2016-09-18 12:01:25 -0400

    update the code from home

--- src/main/java/utility/Log.java
+++ src/main/java/utility/Log.java
@@ -1,6 +1,7 @@
 package utility;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.lf5.util.StreamUtils;
 
 /**
  * Created by wu on 9/18/2016.
@@ -17,5 +18,26 @@ public class Log {
         Log.info("$$$$$$       "+sTestCaseName+"     $$$$$");
         Log.info("****************************************");
         Log.info("****************************************");
+    }
+    //This is to print log for the ending of the test case
+    public static void endTestCase(String sTestCaseName){
+        Log.info("XXXXXXXXXXXXXX    "+"E--N--D"+"   XXXXXX");
+        Log.info("X");
+        Log.info("X");
+        Log.info("X");
+        Log.info("X");
+    }
+    //need to create these method, so that they an be called
+    public static void info(String message){
+        Log.info(message);
+    }
+    public static void warn(String message){
+        Log.warn(message);
+    }
+    public static void error(String message){
+        Log.error(message);
+    }
+    public static void debug(String message){
+        Log.debug(message);
     }
 }
