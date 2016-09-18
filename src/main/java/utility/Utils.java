03588b6277b8af6095d63660928e2fc73ac69f60
amy <wuamyamy@gmail.com>
2016-09-18 12:32:59 -0400

    update the code from home

--- src/main/java/utility/Utils.java
+++ src/main/java/utility/Utils.java
@@ -25,7 +25,27 @@ public class Utils {
                 driver = new FirefoxDriver();
                 Log.info("New driver instantiated");
                 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+                Log.info("Implicit wait applied on the dirver for 10 second");
+                driver.get(Constant.URL);
+                Log.info("Web application launched successfully");
             }
+        } catch (Exception e){
+            Log.error("Class Utils | Method OpenBrowser | Exception desc " +e.getMessage());
+        }
+        return driver;
+    }
+
+    public static String getTestCaseName(String sTestCase) throws Exception {
+        String value = sTestCase;
+        try{
+            int posi = value.indexOf("@");
+            value = value.substring(0, posi);
+            posi = value.lastIndexOf(".");
+            value = value.substring(posi + 1);
+            return value;
+        } catch (Exception e){
+            Log.error("Class Utils | Method getTestCaseName |Exception desc : "+e.getMessage());
+            throw (e)
         }
     }
 }
