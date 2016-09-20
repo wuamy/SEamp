ef92949e390dc5797e05424a2b69a4632e437631
amy <wuamyamy@gmail.com>
2016-09-20 05:27:57 -0400

    update the code from home

--- src/main/java/pageObjects/Home_Page.java
+++ src/main/java/pageObjects/Home_Page.java
@@ -1,10 +1,40 @@
 package pageObjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utility.Log;
+import utility.Utils;
 
 
 /**
  * Created by amy on 9/19/2016.
  */
-public class Home_Page {
+public class Home_Page extends BaseClass {
+    private static WebElement element = null;
+    public Home_Page(WebDriver driver){
+        super(driver);
+
+    }
+    public static WebElement lnk_MyAccount() throws Exception{
+        try {
+            element = driver.findElement(By.cssSelector(".account_icon"));
+            Log.info("My Account link is found on the home page");
+        } catch (Exception e){
+            Log.error("My account link is not found on the home page!");
+            throw (e);
+        }
+        return element;
+    }
+
+    public static WebElement lnk_LogOut(WebDriver driver){
+        try {
+            element = driver.findElement(By.cssSelector("#account_logout>a"));
+            Log.info("Log out link element is found");
+        } catch (Exception e){
+            Log.error("Log Out link is not found on the Home Page");
+            throw (e);
+        }
+        return element;
+    }
 }
---
+++ src/main/java/pageObjects/LogIn.java
@@ -0,0 +1,7 @@
+package pageObjects;
+
+/**
+ * Created by wu on 9/20/2016.
+ */
+public class LogIn {
+}
