3f6cdfa6e40aecc346baba5bfd4796f452deebc8
wuamy <wuamyamy@gmail.com>
2016-09-22 07:54:00 -0400

    edit it from office

--- src/main/java/appModules/SignIn_Action.java
+++ src/main/java/appModules/SignIn_Action.java
@@ -15,4 +15,24 @@ import utility.Utils;
 // Module. for Modular driven framework, please see
 // http://toolsqa.wpengine.com/selenium-webdriver/modular-driven/
 public class SignIn_Action {
+    //iTestcaseRow is the row number of our Testcase name in the Test Data sheet
+    //iTestcaseRow is passed as an argument to this method, so that it can used
+    //inside this method. for use of functions, parameters. please check
+    //http://toolsqa.wpengine.com/selenium-webdriver/function-parameters/
+
+    public static void Execute(int iTestCaseRow) throws Exception{
+        //Clicking on the my account link on the home page
+        Home_Page.lnk_MyAccount().click();
+        Log.info("Click action is performed on my account link");
+        //Storing the username in to a string variable and getting the username from test
+        //data excel sheet
+        //iTestcaseRow is the row nmber of our testcase name in the test data sheet
+        //Constant.col_username is the colum number for username column in the test data sheet
+        //please see the constant class in the utility package
+        //for use of constant variables, please see http://toolsqa.wpengine.com/selenium-webdriver/constant-variables/
+        String sUserName = ExcelUtils.getCellData(iTestCaseRow, Constant.Col_UserName);
+        //here we are sending the username string to the username textbox on the LogIN page
+
+    }
+
 }
